497ab966a07ee0af9340d9fa6c9af6c8a4d86afb
Jimmy <aungphyothant291000@gmail.com>
2025-12-14 13:08:23 +0630

    changed BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Featured from "./components/Featured";
 import About from "./components/About";
